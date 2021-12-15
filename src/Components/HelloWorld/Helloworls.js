a207bd78df6ed7be404cf69c4ae6c44be5fa67cb
Arezoo Moradi <moradi.arezoo1992@gmail.com>
2021-12-15 10:59:05 +0330

    first step:)

---
+++ src/Components/HelloWorld/Helloworls.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Helloworls() {
+    return (
+        <div>
+          <canvas></canvas>
+        </div>
+    )
+}
+
+export default Helloworls
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Helloworls from '../HelloWorld/Helloworls'
+
+const Main = () => {
+    return (
+        <div>
+           <Helloworls /> 
+        </div>
+    )
+}
+
+export default Main
