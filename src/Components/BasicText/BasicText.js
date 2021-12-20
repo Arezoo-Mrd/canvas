7995b78a5e93b857f97a48e6f48d0e7fe0b183db
Arezoo Moradi <moradi.arezoo1992@gmail.com>
2021-12-20 12:24:01 +0330

    add font style in canvas

--- src/Components/BasicText/BasicText.js
+++ src/Components/BasicText/BasicText.js
@@ -8,24 +8,31 @@ const BasicText = () => {
         
         // context.fillText(text, xCoord, yCoord, maxWidth);
         // context.strokeText(text, xCoord, yCoord, maxWidth);
+        // context.font='font-style font-weight font-size font-family';
         
-       context.font='45px Verdana';
+      
         
 
         let fText = "Fill Text on Canvas"
+        // add font style
+        context.font='normal bloder xx-large courier';
         context.fillText(fText, 80, 100);
 
+        context.font='italic 700 48px fantasy';
+        context.fillText(fText, 80, 200);
+
+
+
+
         let sText = 'Stroke Text on Canvas'
-        context.strokeText(sText, 80, 200);
+        context.strokeText(sText, 80, 400);
         
 
         
     }, [])
 
     return (
-        <canvas width="900" height="450" ref={canvas}>
-            
-        </canvas>
+        <canvas width="900" height="450" ref={canvas}></canvas>
     );
 };
 export default BasicText;
